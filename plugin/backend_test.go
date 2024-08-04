a2a89b92824613413efb2a1c35995812eda34652
anatolii2makaroff <anatolii.makaroff@gmail.com>
2024-08-04 18:50:17 -0400

    test path in separate files

--- plugin/backend_test.go
+++ plugin/backend_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/sdk/logical"
-	"github.com/stretchr/testify/require"
 )
 
 
@@ -25,31 +24,3 @@ func getTestBackend(tb testing.TB) (*QdrantBackend, logical.Storage) {
 
 	return b.(*QdrantBackend), config.StorageView
 }
-
-
-// testEnv creates an object to store and track testing environment
-// resources
-type testEnv struct {
-	URL      string
-	Backend logical.Backend
-	Context context.Context
-	Storage logical.Storage
-}
-
-// AddConfig adds the configuration to the test backend.
-// Make sure data includes all of the configuration
-// attributes you need and the `config` path!
-func (e *testEnv) AddConfig(t *testing.T) {
-	req := &logical.Request{
-		Operation: logical.CreateOperation,
-		Path:      "config",
-		Storage:   e.Storage,
-		Data: map[string]interface{}{
-			"url":      e.URL,
-		},
-	}
-	resp, err := e.Backend.HandleRequest(e.Context, req)
-	require.Nil(t, resp)
-	require.Nil(t, err)
-}
-
